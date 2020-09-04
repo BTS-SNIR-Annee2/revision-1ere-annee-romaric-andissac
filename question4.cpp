ee0bc289a673c6916170087da8f43cd8d7e3e0e2
Andissac <andissac.romaric@lyceeppr.fr>
2020-09-04 15:45:33 +0200

    question4

---
+++ question4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+
+    std::string nom; 
+
+    std::cout <<  "machine : Quel est votre pernom ? \nutilisateur : ";
+    std::cin >> nom;
+    std::cout << "machine : Bonjour, " << nom << " !\n";
+    
+}
