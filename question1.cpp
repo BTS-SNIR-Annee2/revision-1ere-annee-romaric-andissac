49fe0e7a8054f62cb16e4ffe929cc8d7391db34d
Andissac <andissac.romaric@lyceeppr.fr>
2020-09-04 15:03:40 +0200

    question1

--- question1.cpp
+++ question1.cpp
@@ -2,5 +2,15 @@
 
 int main() {
 
+    int a = 12;
+    int b = 5;
+    int temp;
     
+    std::cout << "avant : " << a << " " << b << "\n";
+    
+    temp = a;
+    a = b;
+    b = temp;
+    
+    std::cout << "apres : " << a << " " << b  << "\n";
 }
