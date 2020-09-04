d2f7b3d3b962965e9413b40b240077819403389a
Andissac <andissac.romaric@lyceeppr.fr>
2020-09-04 16:58:04 +0200

    question7

---
+++ question7.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int nbUser1;
+    int nbUser2;
+    
+    cout << "Entrez un premier nombre: ";
+    cin >> nbUser1;
+    
+    cout << "Entrez un second nombre: ";
+    cin>> nbUser2;
+    
+   if(nbUser1 < 0 and nbUser2 > 0){
+        cout << "negatif" << endl;
+   }else if(nbUser1 > 0 and nbUser2 < 0){
+        cout << "negatif" << endl;
+   }else{
+        cout << "positif" << endl;
+   }
+}
