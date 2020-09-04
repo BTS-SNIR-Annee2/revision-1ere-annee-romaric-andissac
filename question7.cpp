4e3f467584736e8d69ceeafef34e5d2f1c059bce
Andissac <andissac.romaric@lyceeppr.fr>
2020-09-04 17:00:52 +0200

    question7

--- question7.cpp
+++ question7.cpp
@@ -13,11 +13,13 @@ int main() {
     cout << "Entrez un second nombre: ";
     cin>> nbUser2;
     
-   if(nbUser1 < 0 and nbUser2 > 0){
+    if((nbUser1 == 0) or (nbUser2 == 0)){
+        cout << "null" << endl;
+    }
+    else if((nbUser1 < 0 and nbUser2 > 0) or (nbUser1 > 0 and nbUser2 < 0)) {
         cout << "negatif" << endl;
-   }else if(nbUser1 > 0 and nbUser2 < 0){
-        cout << "negatif" << endl;
-   }else{
+    }
+    else{
         cout << "positif" << endl;
-   }
+    }
 }
